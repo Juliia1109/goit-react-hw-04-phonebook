e6afe88fb0fea5c7489ca04fc1bcbc94fdb175e2
Yuliia Cherkashyna <jul11091109@gmail.com>
2022-12-15 22:34:15 +0200

    form

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -16,22 +16,22 @@ export default function ContactForm ({ onSubmit }) {
           setNumber('');
       }
 
-      // const handleChange= e =>{
-      //   const { name, value } = e.target;
+      const handleChange= e =>{
+        const { name, value } = e.target;
    
-      //   switch (name) {
-      //     case 'name':
-      //       setName(value);
-      //       break;
+        switch (name) {
+          case 'name':
+            setName(value);
+            break;
     
-      //     case 'number':
-      //       setNumber(value);
-      //       break;
+          case 'number':
+            setNumber(value);
+            break;
     
-      //     default:
-      //       break;
-      //   }
-      // };
+          default:
+            break;
+        }
+      };
       
         return (
         <form onSubmit={handleSubmit} className={css.form}>
@@ -41,7 +41,7 @@ export default function ContactForm ({ onSubmit }) {
          type="text"
          name="name"
          value={ name }
-         onChange={e => setName(e.target.value)}
+         onChange={handleChange}
          className={css.input}
          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
          title="Name may contain only letters, apostrophe, dash        and spaces. For example Adrian, Jacob Mercer, Charles de        Batz de Castelmore d'Artagnan"
@@ -54,7 +54,7 @@ export default function ContactForm ({ onSubmit }) {
          type="tel"
          name="number"
          value={ number }
-         onChange={e => setNumber(e.target.value)}
+         onChange={handleChange}
          className={css.input}
          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.       \s]?\d{1,4}[-.\s]?\d{1,9}"
          title="Phone number must be digits and can contain        spaces, dashes, parentheses and can start with +"
